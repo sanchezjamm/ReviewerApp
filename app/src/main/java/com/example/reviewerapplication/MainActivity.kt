9a92ebb7cc929bccc3ddd10b1c6dfc474194e860
Jamaica Sanchez <sanchez.jamaica@auf.edu.ph>
2022-10-07 21:04:39 +0800

    XML File of Reviewer App

---
+++ app/src/main/java/com/example/reviewerapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.reviewerapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+klshyfs
+        fjklsdfjlas
+        ,cl;sjf
+        nfkljsbf
